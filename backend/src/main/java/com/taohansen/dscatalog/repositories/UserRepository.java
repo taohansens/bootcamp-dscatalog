c8811264b3156483539a60e6dbee7e9815ec73c2
taohansens <taohansens@gmail.com>
2023-08-16 11:59:46 -0300

    UserDetailsProjection, Repository, Service Checklist done.

--- backend/src/main/java/com/taohansen/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/taohansen/dscatalog/repositories/UserRepository.java
@@ -1,10 +1,24 @@
 package com.taohansen.dscatalog.repositories;
 
 import com.taohansen.dscatalog.entities.User;
+import com.taohansen.dscatalog.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Query(nativeQuery = true, value = """
+            SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority 
+            FROM tb_user
+            INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+            INNER JOIN tb_role ON tb_user_role.role_id = tb_role.id
+            WHERE tb_user.email = :email
+            """)
+    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
 }
--- backend/src/main/java/com/taohansen/dscatalog/services/UserService.java
+++ backend/src/main/java/com/taohansen/dscatalog/services/UserService.java
@@ -6,6 +6,7 @@ import com.taohansen.dscatalog.dto.UserInsertDTO;
 import com.taohansen.dscatalog.dto.UserUpdateDTO;
 import com.taohansen.dscatalog.entities.Role;
 import com.taohansen.dscatalog.entities.User;
+import com.taohansen.dscatalog.projections.UserDetailsProjection;
 import com.taohansen.dscatalog.repositories.RoleRepository;
 import com.taohansen.dscatalog.repositories.UserRepository;
 import com.taohansen.dscatalog.services.exceptions.DatabaseException;
@@ -15,18 +16,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository repository;
     @Autowired
@@ -88,5 +93,21 @@ public class UserService {
             Role role = roleRepository.getReferenceById(roleDTO.getId());
             entity.getRoles().add(role);
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
     }
 }
