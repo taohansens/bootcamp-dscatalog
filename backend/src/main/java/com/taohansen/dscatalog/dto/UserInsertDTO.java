788f25f1112584409e98b9e0c16b686d347b9d19
taohansens <taohansens@gmail.com>
2023-08-27 16:46:02 -0300

    Adjust Role. Env. EmailConfigs

--- backend/src/main/java/com/taohansen/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/taohansen/dscatalog/dto/UserInsertDTO.java
@@ -1,9 +1,17 @@
 package com.taohansen.dscatalog.dto;
 
 import com.taohansen.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO {
+
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 8, message = "Deve ter no mínimo 8 caracteres")
+    @Pattern(regexp = "^(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$",
+            message = "A senha não é forte o suficiente")
     private String password;
 
     UserInsertDTO() {
---
+++ backend/src/main/java/com/taohansen/dscatalog/services/exceptions/EmailException.java
@@ -0,0 +1,8 @@
+package com.taohansen.dscatalog.services.exceptions;
+
+public class EmailException extends RuntimeException {
+
+	public EmailException(String msg) {
+		super(msg);
+	}
+}
